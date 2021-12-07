eea6df32303f6bb8aee21d60d02fc9772c0b9b09
Francisco <ip4c0@icloud.com>
2021-12-07 16:35:28 -0600

    Added the login component and started setting it up

---
+++ client/src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=957bf69ffd504c589846aa047f026a20&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public%20user-top-read%20user-library-read%20user-library-modify";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
