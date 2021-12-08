fe1971d36271e2f1a535ac8705f2575e8296dde8
Francisco <ip4c0@icloud.com>
2021-12-08 11:06:19 -0600

    Setting the access token to the spotify-web-api-node instance to hmake the requests

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,8 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
 import useAuth from "./useAuth";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyRequest = new SpotifyWebApi({
+  clientId: "957bf69ffd504c589846aa047f026a20",
+});
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  console.log(accessToken);
+
+  // Set access token to the spotify api instance to make the requests
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyRequest.setAccessToken(accessToken);
+    console.log(spotifyRequest);
+  }, [accessToken]);
+
   return <div>{code}</div>;
 }
