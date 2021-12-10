1d0e21316943f80f888b35857a5a30e5421abe16
Francisco <ip4c0@icloud.com>
2021-12-09 19:56:45 -0600

    Basic structure to get the data and set it on state

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "./useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
 
@@ -8,12 +8,62 @@ const spotifyRequest = new SpotifyWebApi({
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
+  const [userProfileData, setUserProfileData] = useState();
+  const [userPlaylists, setUserPlaylists] = useState();
+  const [userFollowing, setUserFollowing] = useState();
+  const [userTopArtists, setUserTopArtists] = useState();
+  const [userTopTracks, setUserTopTracks] = useState();
 
   // Set access token to the spotify api instance to make the requests
   useEffect(() => {
     if (!accessToken) return;
+
     spotifyRequest.setAccessToken(accessToken);
-    console.log(spotifyRequest);
+
+    spotifyRequest.getMe().then(
+      function (data) {
+        setUserProfileData(data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getUserPlaylists("gex0o3cxxwnlq7yuafrysatek").then(
+      function (data) {
+        setUserPlaylists(data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getFollowedArtists({ limit: 1 }).then(
+      function (data) {
+        setUserFollowing(data.body.artists.total);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getMyTopArtists({ time_range: "long_term", limit: 10 }).then(
+      function (data) {
+        setUserTopArtists(data.body.items);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getMyTopTracks({ time_range: "long_term", limit: 10 }).then(
+      function (data) {
+        setUserTopTracks(data.body.items);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
   }, [accessToken]);
 
   return <div>{code}</div>;
