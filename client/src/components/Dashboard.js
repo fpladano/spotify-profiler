a18e127d18abb29a6d23d335782e07f2ff248462
Francisco <ip4c0@icloud.com>
2022-03-02 13:41:41 -0600

    Update the dashboard to handle the auth token and pass it down to the other components

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import useAuth from "../useAuth";
-import SpotifyWebApi from "spotify-web-api-node";
 import Nav from "./Nav";
-import { Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import User from "./User";
 import Artists from "./Artists";
@@ -10,72 +9,14 @@ import Tracks from "./Tracks";
 import Recent from "./Recent";
 import Playlist from "./Playlists";
 
-const spotifyRequest = new SpotifyWebApi({
-  clientId: "957bf69ffd504c589846aa047f026a20",
-});
-
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  const [userProfileData, setUserProfileData] = useState(null);
-  const [userPlaylists, setUserPlaylists] = useState(null);
-  const [userFollowing, setUserFollowing] = useState(null);
-  const [userTopArtists, setUserTopArtists] = useState(null);
-  const [userTopTracks, setUserTopTracks] = useState(null);
+  const [accessTokenState, setAccessTokenState] = useState(null);
 
   // Set access token to the spotify api instance to make the requests
   useEffect(() => {
     if (!accessToken) return;
-
-    //Setting up the access token to my spotify request instance
-    spotifyRequest.setAccessToken(accessToken);
-
-    //The requests to get the data needed on the dashboard
-
-    //Getting user info
-    spotifyRequest.getMe().then(
-      function (data) {
-        setUserProfileData(data.body);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
-
-    spotifyRequest.getUserPlaylists("gex0o3cxxwnlq7yuafrysatek").then(
-      function (data) {
-        setUserPlaylists(data.body);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
-
-    spotifyRequest.getFollowedArtists({ limit: 1 }).then(
-      function (data) {
-        setUserFollowing(data.body.artists.total);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
-
-    spotifyRequest.getMyTopArtists({ time_range: "long_term", limit: 10 }).then(
-      function (data) {
-        setUserTopArtists(data.body.items);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
-
-    spotifyRequest.getMyTopTracks({ time_range: "long_term", limit: 10 }).then(
-      function (data) {
-        setUserTopTracks(data.body.items);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
+    setAccessTokenState(accessToken);
   }, [accessToken]);
 
   return (
@@ -84,7 +25,7 @@ export default function Dashboard({ code }) {
         <Nav />
         <div className="w-full h-full bg-spotifyBlack">
           <Routes>
-            <Route path="/" element={<User />} />
+            <Route path="/" element={<User token={accessTokenState} />} />
             <Route path="/artists" element={<Artists />} />
             <Route path="/tracks" element={<Tracks />} />
             <Route path="/recent" element={<Recent />} />
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,5 +1,78 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
 
-export default function User() {
-  return <div className="text-xl text-white">Welcome</div>;
+const spotifyRequest = new SpotifyWebApi({
+  clientId: "957bf69ffd504c589846aa047f026a20",
+});
+
+export default function User({ token }) {
+  const [userProfileData, setUserProfileData] = useState(null);
+  const [userPlaylists, setUserPlaylists] = useState(null);
+  const [userFollowing, setUserFollowing] = useState(null);
+  const [userTopArtists, setUserTopArtists] = useState(null);
+  const [userTopTracks, setUserTopTracks] = useState(null);
+
+  useEffect(() => {
+    if (!token) return;
+
+    spotifyRequest.setAccessToken(token);
+
+    spotifyRequest.getMe().then(
+      function (data) {
+        setUserProfileData(data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getUserPlaylists("gex0o3cxxwnlq7yuafrysatek").then(
+      function (data) {
+        setUserPlaylists(data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getFollowedArtists({ limit: 1 }).then(
+      function (data) {
+        setUserFollowing(data.body.artists.total);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getMyTopArtists({ time_range: "long_term", limit: 10 }).then(
+      function (data) {
+        setUserTopArtists(data.body.items);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyRequest.getMyTopTracks({ time_range: "long_term", limit: 10 }).then(
+      function (data) {
+        setUserTopTracks(data.body.items);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+  }, [token]);
+
+  return (
+    <main>
+      <header>
+        <div>
+          <i>Profile Pic</i>
+        </div>
+        <a>Profile name link to profile in spotify</a>
+        <div>user stats like followes...</div>
+        <a>logout btn</a>
+      </header>
+    </main>
+  );
 }
